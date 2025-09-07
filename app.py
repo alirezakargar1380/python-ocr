d341d200adb0a238cd025f8ae43904fd0954aee3
alireza kargar <alirezakargarsuport@gmail.com>
2025-09-07 21:45:08 +0330

    add extract API

--- app.py
+++ app.py
@@ -3,21 +3,27 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
 import shutil
 from pathlib import Path
+import time
+from PIL import Image
+
+POST_URL = "https://tracking.post.ir"
+
 
 def extract_numbers_from_image(image_path: str):
     # Initialize the reader (English by default)
-    reader = easyocr.Reader(['en'])
-    
+    reader = easyocr.Reader(["en"])
+
     # Run OCR on the image
     results = reader.readtext(image_path, detail=0)  # detail=0 gives only text
-    
+
     # Combine all detected text into a single string
     text = " ".join(results)
     return text
 
 
 app = FastAPI()
-app.mount('/api', app)
+app.mount("/api", app)
+
 
 @app.post("/extract-number")
 async def extract_number_api(file: UploadFile = File(...)):
@@ -30,3 +36,7 @@ async def extract_number_api(file: UploadFile = File(...)):
         temp_path.unlink(missing_ok=True)
     return JSONResponse(content={"number": result})
 
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
